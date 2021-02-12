885a07d09fd79f0dcfe5a68bd8ac4551718473bc
jthanawat <kla13thanawat@gmail.com>
2021-02-12 22:10:47 +0700

    add Dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
