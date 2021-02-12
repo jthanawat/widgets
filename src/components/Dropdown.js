5ef1e9fa2e5d85803239a2ad52a826e8cd336238
jthanawat <kla13thanawat@gmail.com>
2021-02-12 23:55:48 +0700

    hide when click anywhere

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef, useReducer } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener(
+      'click',
+      (event) => {
+        if (ref.current && ref.current.contains(event.target)) {
+          return;
+        }
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
+
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -17,8 +32,9 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       </div>
     );
   });
+
   return (
-    <div className="ui form">
+    <div className="ui form" ref={ref}>
       <div className="field">
         <label className="label">Select a Color</label>
         <div
